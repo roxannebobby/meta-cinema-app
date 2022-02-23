83a468a1aa9dad9f5864782d2a00d3df0e63aef0
Roxanne Weber <marie.sankofa@gmail.com>
2022-02-23 17:09:25 -0600

    user product and order models added

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-import products from './data/products.js';
 
 dotenv.config();
 connectDB();
 const app = express();
 
+// @ROUTES
+import productRoutes from './routes/productRoutes.js'
+
+
+app.use('/api/concessions', productRoutes)
+
 app.get('/', (req, res) => {
 	res.send('API is running...');
 });
 
-app.get('/api/concessions', (req, res) => {
-	res.json(products);
-});
 
-app.get('/api/concessions/:id', (req, res) => {
-	const product = products.find((p) => p._id === req.params.id);
-	res.json(product);
-});
 
 const PORT = process.env.PORT || 5000;
 
