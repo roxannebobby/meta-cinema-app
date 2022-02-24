d2a44e5d677257ea048c2aa88f400911af766c52
Roxanne Weber <marie.sankofa@gmail.com>
2022-02-24 01:36:07 -0600

    thunk wired and working

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,11 +3,15 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 const reducer = combineReducers({});
+
 const initialState = {};
 
 const middleware = [thunk];
+
 const store = createStore(
 	reducer,
 	initialState,
-	composeWithDevTools(applyMiddleware([...middleware]))
+	composeWithDevTools(applyMiddleware(...middleware))
 );
+
+export default store;
