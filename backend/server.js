de0155752d8534d0ffc68bbab815b4afc8d3d2d7
Roxanne Weber <marie.sankofa@gmail.com>
2022-02-23 23:52:21 -0600

    error middleware wired and working

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import colors from 'colors';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 // @ROUTES
 import productRoutes from './routes/producutRoutes.js';
@@ -17,6 +18,10 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+// @error handling
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
