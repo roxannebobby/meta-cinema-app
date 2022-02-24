eb33e8831af8244fcb19f9a262a2f50027f41eee
Roxanne Weber <marie.sankofa@gmail.com>
2022-02-23 22:20:24 -0600

    products pulled from backend js file

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const products = require('./data/products.js');
+import express from 'express';
+import products from './data/products.js';
 const app = express();
 
 app.get('/', (req, res) => {
--- frontend/src/screens/ConcessionsScreen.js
+++ frontend/src/screens/ConcessionsScreen.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
-import products from '../products';
 import Product from '../components/Product';
 
 const ConcessionScreen = () => {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		const getProducts = async (req, res) => {
+			const { data } = await axios.get('/api/products');
+			setProducts(data);
+		};
+		getProducts();
+	}, []);
+
 	return (
 		<>
 			<h1>Concessions</h1>
 			<Row>
-			{products.map((product) => (
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
+				{products.map((product) => (
+					<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+						<Product product={product} />
 					</Col>
 				))}
 			</Row>
