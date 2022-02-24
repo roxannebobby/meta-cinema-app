85f5991ba316416e0f4507b8ee25c91921bf5131
Roxanne Weber <marie.sankofa@gmail.com>
2022-02-23 22:24:46 -0600

    data being pulled fully from backend only

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
+import axios from 'axios';
 
 const ProductScreen = ({ match }) => {
-	const product = products.find((p) => p._id === match.params.id);
+	const [product, setProduct] = useState({});
+
+	useEffect(() => {
+		const getProduct = async (req, res) => {
+			const { data } = await axios.get(`/api/products/${match.params.id}`);
+			setProduct(data);
+		};
+		getProduct();
+	}, []);
 
 	return (
 		<>
@@ -57,7 +65,11 @@ const ProductScreen = ({ match }) => {
 							</ListGroup.Item>
 							<ListGroup.Item>
 								<Row>
-									<Button className='btn-block' type='button' disabled={product.countInStock === 0}>
+									<Button
+										className='btn-block'
+										type='button'
+										disabled={product.countInStock === 0}
+									>
 										Add to Cart
 									</Button>
 								</Row>
