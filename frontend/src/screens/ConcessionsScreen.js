7ea8d9c7f60fd0cc34869a9070ac9b4b3da1a63b
Roxanne Weber <marie.sankofa@gmail.com>
2022-02-24 02:36:31 -0600

    loader and message component wired and working

--- frontend/src/screens/ConcessionsScreen.js
+++ frontend/src/screens/ConcessionsScreen.js
@@ -3,6 +3,8 @@ import React, { useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Product from '../components/Product';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
 import { listProducts } from '../actions/productActions';
 
 const ConcessionScreen = () => {
@@ -19,9 +21,9 @@ const ConcessionScreen = () => {
 		<>
 			<h1>Concessions</h1>
 			{loading ? (
-				<h2>Loading...</h2>
+				<Loader />
 			) : error ? (
-				<h3>{error}</h3>
+				<Message variant='danger'>{error}</Message>
 			) : (
 				<Row>
 					{products.map((product) => (
@@ -31,7 +33,6 @@ const ConcessionScreen = () => {
 					))}
 				</Row>
 			)}
-			}
 		</>
 	);
 };
