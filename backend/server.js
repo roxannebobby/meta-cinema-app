e0573b9a85115ad8410bec88996ebdb952cf47b5
Roxanne Weber <marie.sankofa@gmail.com>
2022-02-23 21:58:24 -0600

    products wired in backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,19 @@
-const express = require('express')
+const express = require('express');
+const products = require('./data/products.js');
+const app = express();
 
-const app = express()
+app.get('/', (req, res) => {
+	res.send('API is running...');
+});
 
-app.listen(5000, console.log('Server running on port 5000'))
+app.get('/api/products', (req, res) => {
+	res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+	const product = products.find((p) => p._id === req.params.id);
+
+	res.json(products);
+});
+
+app.listen(5000, console.log('Server running on port 5000'));
